fc37a0aa133fe044263c1e1f9d45ace49b9ad385
nandihabba <30462237+nandihabba@users.noreply.github.com>
2019-06-13 07:20:28 +0530

    mesg'

--- assets/js/pride_display.js
+++ assets/js/pride_display.js
@@ -9,7 +9,7 @@ angular.module('CrisApp', [])
             Name: "Alfred",
             Position: "Butler",
             BIB: "Batman",
-			link: 'http://yogi.run/assets/img/gallery/img-1.jpg',
+			link: 'http://yogi.run/assets/img/nandiwinner/5k_1_openmen.jpg',
             Color: "red"
         },
         {
@@ -17,21 +17,21 @@ angular.module('CrisApp', [])
             Occupation: "Queen of Jungle",
             Employer: "Tarzan",
           
-          link: 'http://yogi.run/assets/img/gallery/img-2.jpg',
+			link: 'http://yogi.run/assets/img/nandiwinner/21k_1_openmen.jpg',
         },
         {
             Name: "Aladdin",
             Occupation: "Beggar Prince",
             Employer: "Self-Employed",
           
-          link: 'http://yogi.run/assets/img/gallery/img-3.jpg',
+			link: 'http://yogi.run/assets/img/nandiwinner/21k_2_openmen.jpg',
         },
         {
             Name: "Bill Gates",
             Occupation: "Bajillionaire",
             Employer: "Microsoft",
           
-          link: 'https://s-media-cache-ak0.pinimg.com/originals/2d/fb/d3/2dfbd38983291d3d0e5c1cb0667406b5.jpg',
+       link: 'http://yogi.run/assets/img/nandiwinner/21k_3_openmen.jpg',
         },
         {
             Name: "Manny",
